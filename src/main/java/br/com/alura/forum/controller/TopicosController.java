24a0b891d0d775253dcf625e6107b4199f48ab08
Crummorn <igorcaze@gmail.com>
2019-09-22 14:15:01 -0300

    Adicionado Paginação

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,13 +1,16 @@
 package br.com.alura.forum.controller;
 
 import java.net.URI;
-import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -38,13 +42,14 @@ public class TopicosController {
 	private CursoRepository cursoRepository;
 
 	@GetMapping
-	public List<TopicoDto> lista(String curso) {
-		List<Topico> topicos;
+	public Page<TopicoDto> lista(@RequestParam(required = false) String nomeCurso,
+			@PageableDefault(size = 10, sort = "id") Pageable pageable) {
+		Page<Topico> topicos;
 
-		if (curso == null) {
-			topicos = topicoRepository.findAll();
+		if (nomeCurso == null) {
+			topicos = topicoRepository.findAll(pageable);
 		} else {
-			topicos = topicoRepository.findByCursoNome(curso);
+			topicos = topicoRepository.findByCursoNome(nomeCurso, pageable);
 		}
 
 		return TopicoDto.converter(topicos);
