5a247f3ab86aa6bd92852114885da2a7bb3950f9
Crummorn <igorcaze@gmail.com>
2019-09-25 16:27:41 -0300

    Adicionado SpringSecurity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$jdAXXgpx/l.pDiNM/6y4DeH/uW3CtnhbeHaX9DjNlwEqZp8z4XAc.');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
